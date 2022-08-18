ca6fc47891e32433f469de6ab7a3044edfe41385
emcon84 <emiliano@oliver.pet>
2022-08-17 22:22:39 -0300

    test2

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -29,7 +29,7 @@ export const LoginPage = () => {
         dispatch(checkingSignInFromGoogle())
     }
 
-    ///probando cambios
+
     return (
         <AuthLayout title='Login!!!'>
             <form onSubmit={onSubmit} className='animate__animated animate__fadeIn animate__faster'>
